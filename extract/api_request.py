8b068cd8fe6f6de3ff6d13a621320242cd26c5e1
Martynas Stelmokas <stelmokas00@gmail.com>
2024-10-30 18:43:32 +0200

    Created extraction layer

--- extract/api_request.py
+++ extract/api_request.py
@@ -4,45 +4,40 @@ import os
 from concurrent.futures import ThreadPoolExecutor
 import asyncio
 from typing import Dict
+import aiohttp
+
 
 class ApiRequest:
     """
     A class to handle synchronous and asynchronous requests to the Gold API.
 
     Attributes:
-        ticker (str): The ticker symbol representing the asset to query from the API.
+        url (str): The URL to query from the API.
+        header (Dict): Optional headers for the request.
+        params (Dict): Optional parameters for the request.
     """
 
-    def __init__(self, url: str, header: Dict = {}):
-        """
-        Initializes the ApiRequest instance with a specified ticker symbol.
-
-        Args:
-            ticker (str): The ticker symbol for the asset (e.g., 'XAU' for gold).
-        """
+    def __init__(self, url: str, header: Dict = {}, params: Dict = {}):
         self.url = url
         self.header = header
+        self.params = params
 
     def make_request(self):
-        """
-        Makes a synchronous HTTP GET request to the Gold API using the provided ticker.
-
-        Returns:
-            str: The API response text if the request is successful.
-            None: If an error occurs during the request.
-
-        Raises:
-            requests.exceptions.RequestException: If the HTTP request fails.
-        """
-        
-        url = self.url
-
-        header = self.header
-
+        """Makes a synchronous HTTP GET request to the API."""
         try:
-            response = requests.get(url, headers=header)
+            response = requests.get(self.url, headers=self.header, params=self.params)
             response.raise_for_status()
             return response.text
         except requests.exceptions.RequestException as e:
             print("Error:", str(e))
             return None
+
+    async def async_make_request(self):
+        """Makes an asynchronous HTTP GET request to the API."""
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(self.url, headers=self.header, params=self.params) as response:
+                    response.raise_for_status()
+                    return await response.json()
+            except aiohttp.ClientError as e:
+                return None
