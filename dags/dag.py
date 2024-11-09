11505e6bf70ae8f208749dc642685e1f179199e4
Martynas Stelmokas <stelmokas00@gmail.com>
2024-11-09 16:00:04 +0200

    Docker update to work properly

--- dags/dag.py
+++ dags/dag.py
@@ -3,7 +3,6 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime
 import sys
 import os
-sys.path = [os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))]
 from extraction import fetch_findwork_jobs, fetch_remoteok_jobs, fetch_remotive_jobs
 from transformation import create_df, clean_df, create_statistic
 from load_to_database import insert_to_database
@@ -14,42 +13,50 @@ with DAG(
     schedule_interval='0 0 * * *',
     start_date=datetime(2024, 10, 1),
     end_date=datetime(2024, 12, 1),
-    catchup=False
+    catchup=False,
+    params={}
 ) as dag:
 
     extract_data_api_1 = PythonOperator(
         task_id='fetch_findwork_jobs',
-        python_callable=fetch_findwork_jobs
+        python_callable=fetch_findwork_jobs,
+        params={}
     )
 
     extract_data_api_2 = PythonOperator(
         task_id='fetch_remoteok_jobs',
-        python_callable=fetch_remoteok_jobs
+        python_callable=fetch_remoteok_jobs,
+        params={}
     )
 
     extract_data_api_3 = PythonOperator(
         task_id='fetch_remotive_jobs',
-        python_callable=fetch_remotive_jobs
+        python_callable=fetch_remotive_jobs,
+        params={}
     )
 
     concatenate_data = PythonOperator(
         task_id='concatenate_data',
-        python_callable=create_df
+        python_callable=create_df,
+        params={}
     )
 
     cleaned_df = PythonOperator(
         task_id='cleaned_df',
-        python_callable=clean_df
+        python_callable=clean_df,
+        params={}
     )
 
     created_statistic = PythonOperator(
         task_id='create_statistic',
-        python_callable=create_statistic
+        python_callable=create_statistic,
+        params={}
     )
 
     inserted_to_database = PythonOperator(
         task_id='insert_to_database',
-        python_callable=insert_to_database
+        python_callable=insert_to_database,
+        params={}
     )
 
 [extract_data_api_1, extract_data_api_2, extract_data_api_3] >> concatenate_data >> cleaned_df >> created_statistic >> inserted_to_database
